30f716d37baa29df540970c6c534cb6dbab1a168
SplatterMelon <splattermelon@gmail.com>
2021-09-19 21:26:16 -0400

    added 2500-Lab1 project

---
+++ WorkSpaceFor2500/2500-Lab1/src/CardStack.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CardStack
+{
+	public static void main(String[] args)
+	{
+		Scanner userInput = new Scanner(System.in);
+		int n = userInput.nextInt();
+		int m = userInput.nextInt();
+		
+	}
+	
+	public static int OSVtoInt(String OSV)
+	{
+		return 5;
+	}
+}
