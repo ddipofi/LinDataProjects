8ab67993597afd206d891eecc7e6c19302078822
SplatterMelon <splattermelon@gmail.com>
2021-09-21 19:54:58 -0400

    llnode class and working on validating osv

--- WorkSpaceFor2500/2500-Lab1/src/CardStack.java
+++ WorkSpaceFor2500/2500-Lab1/src/CardStack.java
@@ -7,28 +7,43 @@ import java.util.Scanner;
 
 public class CardStack
 {
-	static int OSVInt = 0;
-	
 	public static void main(String[] args)
 	{
 		Scanner userInput = new Scanner(System.in);
 //		int n = userInput.nextInt();
 //		int m = userInput.nextInt();
 		
-		String OSVValue = userInput.nextLine();
+		String OSV = userInput.nextLine();
 		
-		OSVtoInt(OSVValue);
-		System.out.println(OSVInt);
 	}
 	
 	public static boolean ValidateOSV(String OSV)
 	{
-		return false;
+		boolean valid = false;
+		boolean OValid = false;
+		boolean SValid = false;
+		boolean VValid = false;
+		
+		if (OSV.charAt(0) == 'U' || OSV.charAt(0) == 'u' || OSV.charAt(0) == 'D' || OSV.charAt(0) == 'd')
+		{
+			OValid = true;
+		}
+		if (OSV.charAt(0) == 'C' || OSV.charAt(0) == 'c' || OSV.charAt(0) == 'D' || OSV.charAt(0) == 'd' || OSV.charAt(0) == 'H' || OSV.charAt(0) == 'h' || OSV.charAt(0) == 'S' || OSV.charAt(0) == 's')
+		{
+			SValid = true;
+		}
+		//TODO VValid if statement
+		if (OValid == true && SValid == true && VValid == true)
+		{
+			valid = true;
+		}
+		
+		return valid;
 	}
 	
 	public static int OSVtoInt(String OSV)
 	{
-		int OSVIntt = 0;
+		int OSVInt = 0;
 		char S = OSV.charAt(1);
 		char V = OSV.charAt(2);
 		
@@ -36,20 +51,31 @@ public class CardStack
 		{
 			case 'C':
 				break;
+			case 'c':
+				break;
 			case 'D':
 				OSVInt += 13;
 				break;
+			case 'd':
+				OSVInt += 13;
+				break;	
 			case 'H':
 				OSVInt += 26;
 				break;
+			case 'h':
+				OSVInt += 26;
+				break;
 			case 'S':
+				OSVInt += 39;
+				break;
+			case 's':
 				OSVInt += 39;
 				break;
 			default:
 				break;
 		}
 		
-		if (V != 'T' && V != 'J' && V != 'Q' && V != 'K' && V != 'A')
+		if (V != 'T' && V != 'J' && V != 'Q' && V != 'K' && V != 'A' && V != 't' && V != 'j' && V != 'q' && V != 'k' && V != 'a')
 		{
 			int VInt = V - '0';
 			
@@ -62,16 +88,31 @@ public class CardStack
 				case 'T':
 					OSVInt += 10;
 					break;
+				case 't':
+					OSVInt += 10;
+					break;
 				case 'J':
+					OSVInt += 11;
+					break;
+				case 'j':
 					OSVInt += 11;
 					break;
 				case 'Q':
 					OSVInt += 12;
 					break;
+				case 'q':
+					OSVInt += 12;
+					break;
 				case 'K':
 					OSVInt += 13;
 					break;
+				case 'k':
+					OSVInt += 13;
+					break;
 				case 'A':
+					OSVInt += 1;
+					break;
+				case 'a':
 					OSVInt += 1;
 					break;
 				default:
@@ -79,7 +120,7 @@ public class CardStack
 			}
 		}
 		
-		if (OSV.charAt(0) != 'U')
+		if (OSV.charAt(0) != 'U' && OSV.charAt(0) != 'u')
 		{
 			OSVInt *= -1;
 		}
---
+++ WorkSpaceFor2500/2500-Lab1/src/LLStack.java
@@ -0,0 +1,23 @@
+
+
+public class LLStack<T>
+{
+	LLNode head;
+	
+	private class LLNode<T> {
+		T value;
+		LLNode next;
+		
+		LLNode(T value) {
+			this.value = value;
+		}
+		void setNext(LLNode node) {
+			this.next = node;
+		}
+		T getValue() {
+			return value;
+		}
+	}
+		
+	}
+}
