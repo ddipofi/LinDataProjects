43e6478d94e3f610cae4ad5a8598342d24d06f2c
SplatterMelon <splattermelon@gmail.com>
2021-09-22 23:15:54 -0400

    int to osv method added

--- WorkSpaceFor2500/2500-Lab1/src/CardStack.java
+++ WorkSpaceFor2500/2500-Lab1/src/CardStack.java
@@ -11,13 +11,13 @@ public class CardStack
 	{
 		Scanner userInput = new Scanner(System.in);
 		int n = userInput.nextInt();
-		int m = userInput.nextInt();
+		int m = userInput.nextInt();		
 		
 		for (int i = 1; i <= n * m; i++)
 		{
 			String OSVString = userInput.nextLine();
 			ValidateOSV(OSVString);
-			int OSVInt = OSVtoInt(OSVString);
+			int OSVInt = OSVToInt(OSVString);
 			//add OSVInt to array or node or stack or whatever
 		}
 		
@@ -56,7 +56,7 @@ public class CardStack
 		return valid;
 	}
 	
-	public static int OSVtoInt(String OSV)
+	public static int OSVToInt(String OSV)
 	{
 		int OSVInt = 0;
 		char S = OSV.charAt(1);
@@ -141,5 +141,179 @@ public class CardStack
 		}
 		
 		return OSVInt;
+	}
+	
+	public static String IntToSV(int value)
+	{
+		String OSVString = "";
+		
+		if (value < 0)
+		{
+			value *= -1;
+		}		
+		
+		switch (value)
+		{
+			case 1:
+				OSVString = "CA";
+				break;
+			case 2:
+				OSVString = "C2";
+				break;
+			case 3:
+				OSVString = "C3";
+				break;
+			case 4:
+				OSVString = "C4";
+				break;
+			case 5:
+				OSVString = "C5";
+				break;
+			case 6:
+				OSVString = "C6";
+				break;
+			case 7:
+				OSVString = "C7";
+				break;
+			case 8:
+				OSVString = "C8";
+				break;
+			case 9:
+				OSVString = "C9";
+				break;
+			case 10:
+				OSVString = "CT";
+				break;
+			case 11:
+				OSVString = "CJ";
+				break;
+			case 12:
+				OSVString = "CQ";
+				break;
+			case 13:
+				OSVString = "CK";
+				break;
+			case 14:
+				OSVString = "DA";
+				break;
+			case 15:
+				OSVString = "D2";
+				break;
+			case 16:
+				OSVString = "D3";
+				break;
+			case 17:
+				OSVString = "D4";
+				break;
+			case 18:
+				OSVString = "D5";
+				break;
+			case 19:
+				OSVString = "D6";
+				break;
+			case 20:
+				OSVString = "D7";
+				break;
+			case 21:
+				OSVString = "D8";
+				break;
+			case 22:
+				OSVString = "D9";
+				break;
+			case 23:
+				OSVString = "DT";
+				break;
+			case 24:
+				OSVString = "DJ";
+				break;
+			case 25:
+				OSVString = "DQ";
+				break;
+			case 26:
+				OSVString = "DK";
+				break;
+			case 27:
+				OSVString = "HA";
+				break;
+			case 28:
+				OSVString = "H2";
+				break;
+			case 29:
+				OSVString = "H3";
+				break;
+			case 30:
+				OSVString = "H4";
+				break;
+			case 31:
+				OSVString = "H5";
+				break;
+			case 32:
+				OSVString = "H6";
+				break;
+			case 33:
+				OSVString = "H7";
+				break;
+			case 34:
+				OSVString = "H8";
+				break;
+			case 35:
+				OSVString = "H9";
+				break;
+			case 36:
+				OSVString = "HT";
+				break;
+			case 37:
+				OSVString = "HJ";
+				break;
+			case 38:
+				OSVString = "HQ";
+				break;
+			case 39:
+				OSVString = "HK";
+				break;
+			case 40:
+				OSVString = "SA";
+				break;
+			case 41:
+				OSVString = "S2";
+				break;
+			case 42:
+				OSVString = "S3";
+				break;
+			case 43:
+				OSVString = "S4";
+				break;
+			case 44:
+				OSVString = "S5";
+				break;
+			case 45:
+				OSVString = "S6";
+				break;
+			case 46:
+				OSVString = "S7";
+				break;
+			case 47:
+				OSVString = "S8";
+				break;
+			case 48:
+				OSVString = "S9";
+				break;
+			case 49:
+				OSVString = "ST";
+				break;
+			case 50:
+				OSVString = "SJ";
+				break;
+			case 51:
+				OSVString = "SQ";
+				break;
+			case 52:
+				OSVString = "SK";
+				break;
+			default:
+				break;
+		}
+		
+		return OSVString;
 	}
 }
