dbe183a5f64a029d540f06ccbea1efcfaab393df
SplatterMelon <splattermelon@gmail.com>
2021-09-26 14:27:55 -0400

    added loops for flipping cards and stacking them

--- WorkSpaceFor2500/2500-Lab1/src/CardStack.java
+++ WorkSpaceFor2500/2500-Lab1/src/CardStack.java
@@ -10,23 +10,91 @@ public class CardStack
 		//should be in loop for as many tests as there are
 		int n = userInput.nextInt();
 		int m = userInput.nextInt();
+		int T = 0;
+		int B = n - 1;
+		int L = 0;
+		int R = m - 1;
 		userInput.nextLine();
 		@SuppressWarnings("unchecked")
 		LLStack<Integer>[][] cards = new LLStack[n][m];
 		
-		for (int i = 1; i <= n * m; i++)
+		for (int i = 0; i < n; i++)
 		{
-			String OSVString = userInput.nextLine();
-			OSV.ValidateOSV(OSVString);
-			int OSVInt = OSV.OSVToInt(OSVString);
-			cards[n][m].push(OSVInt);
+			for (int j = 0; j < m; j++)
+			{
+				String OSVString = userInput.nextLine();
+				OSV.ValidateOSV(OSVString);
+				int OSVInt = OSV.OSVToInt(OSVString);
+				cards[i][j] = new LLStack();
+				cards[i][j].push(OSVInt);
+			}
 		}
 		
-		for (int i = 1; i <= n + m - 2; i++)
+		for (int i = 0; i < n + m - 2; i++)
 		{
-			String flipString = userInput.nextLine();
-			//add flipString to array or something to keep track
+			char flipChar = userInput.next().charAt(0);
+			flipChar = Character.toUpperCase(flipChar);
+			
+			switch (flipChar)
+			{
+				case 'T':
+					for (int j = L; j <= R; j++)
+					{
+						while (!cards[T][j].isEmpty())
+						{
+							var card = cards[T][j].top();
+							card *= -1;
+							cards[T+1][j].push(card);
+							cards[T][j].pop();
+						}	
+					}
+					T += 1;
+					break;
+				case 'B':
+					for (int j = L; j <= R; j++)
+					{
+						while (!cards[B][j].isEmpty())
+						{
+							var card = cards[B][j].top();
+							card *= -1;
+							cards[B-1][j].push(card);
+							cards[B][j].pop();
+						}	
+					}
+					B -= 1;
+					break;
+				case 'L':
+					for (int j = T; j <= B; j++)
+					{
+						while (!cards[j][L].isEmpty())
+						{
+							var card = cards[j][L].top();
+							card *= -1;
+							cards[j][L+1].push(card);
+							cards[j][L].pop();
+						}
+					}
+					L += 1;
+					break;
+				case 'R':
+					for (int j = T; j <= B; j++)
+					{
+						while (!cards[j][R].isEmpty())
+						{
+							var card = cards[j][R].top();
+							card *= -1;
+							cards[j][R-1].push(card);
+							cards[j][R].pop();
+						}
+					}
+					R -= 1;
+					break;
+				default:
+					break;
+			}
 		}
+		
+		
 		
 		userInput.close();
 	}
