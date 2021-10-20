e8f2fb8b53790b907f4de424902dc99a7bd76745
SplatterMelon <splattermelon@gmail.com>
2021-10-20 18:36:21 -0400

    made llnode class, deffo wrong

---
+++ WorkSpaceFor2500/2500-Project2/src/LLNode.java
@@ -0,0 +1,43 @@
+public class LLNode
+{
+	protected String word;
+	protected int count;
+	protected LLNode link;
+
+	public LLNode(String word, int count)
+	{
+		this.word = word;
+		this.count = count;
+		link = null;
+	}
+
+	public void setWord(String word)
+	{
+		this.word = word;
+	}
+
+	public String getWord()
+	{
+		return word;
+	}
+
+	public void setCount(int count)
+	{
+		this.count = count;
+	}
+
+	public int getCount()
+	{
+		return count;
+	}
+
+	public void setLink(LLNode link)
+	{
+		this.link = link;
+	}
+
+	public LLNode getLink()
+	{
+		return link;
+	}
+}
