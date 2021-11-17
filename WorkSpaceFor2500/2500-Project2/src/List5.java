9f6817b0516acbb97df0164028834c8df5532655
SplatterMelon <splattermelon@gmail.com>
2021-11-16 22:01:15 -0500

    added slnode stuff

--- WorkSpaceFor2500/2500-Project2/src/List5.java
+++ WorkSpaceFor2500/2500-Project2/src/List5.java
@@ -1,5 +1,37 @@
 
-public class List5
+public class List5 extends BaseList
 {
+	
+	
+	
+	
+	@Override
+	public int getDistinctWords()
+	{
+		// How many nodes are there on the list? Each corresponds to a unique word
+		//
+		int count = 0; // Alternate coding:
+		SLNode p = list; //
+		while (p != null) // for (LLNode p = list; p != null; p = p.getNext()) count++;
+		{ // return count;
+			count++; //
+			p = p.getNext(); //
+		} //
+		return count; //
+	}
 
+	@Override
+	public int getTotalWords()
+	{
+		// How many TOTAL words? That's the sum of the counts in each node.
+		//
+		int count = 0;
+		SLNode p = list;
+		while (p != null)
+		{
+			count += p.count;
+			p = p.getNext();
+		}
+		return count;
+	}
 }
---
+++ WorkSpaceFor2500/2500-Project2/src/SLNode.java
@@ -0,0 +1,24 @@
+
+public class SLNode
+{
+	protected String word;
+	protected int count;
+	protected LLNode link;
+
+	public SLNode(String word, int count) //constructor for node, needs a word and count of how many times this word has appeared in a txt file, as well as a link
+	{
+		this.word = word;
+		this.count = count;
+		link = null;
+	}
+
+	public void setNext(LLNode link) //sets the link
+	{
+		this.link = link;
+	}
+
+	public LLNode getNext()//gets the current link
+	{
+		return link;
+	}
+}
